56acc3b1e3cd4fb77dcad3940c66e456305c5502
beaTejakulator <78610347+beaTejakulator@users.noreply.github.com>
2023-07-04 23:17:37 +0000

    Test

---
+++ tibber-meter-uploader/run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bashio
+
+# Lesen der Konfigurationsoptionen aus options.json
+TIBBER_LOGIN=$(bashio::config 'tibber_login')
+TIBBER_PASSWORD=$(bashio::config 'tibber_password')
+
+
+# Verwendung der Konfigurationsoptionen in Ihrem Add-On
+echo "TIBBER_LOGIN: $TIBBER_LOGIN"
+echo "TIBBER_PASSWORD: $TIBBER_PASSWORD"
+
+# Ausführen Ihrer Java-Anwendung
+java -jar /opt/myapp/tibber-meter-uploader-1.0.0-SNAPSHOT.jar
